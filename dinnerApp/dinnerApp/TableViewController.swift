4df5f8ae645f761101331aece4349a089357cc52
Thomas Miles <tommylmiles@gmail.com>
2019-10-14 01:17:56 -0400

    Updates added
    Core data added
    Fetching data from coredata and ready for display

--- dinnerApp/dinnerApp/TableViewController.swift
+++ dinnerApp/dinnerApp/TableViewController.swift
@@ -7,16 +7,18 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
 
     //let dummyData:[[String]] = [["McDonalds","123 fake street"],["Dominoes", "1114 Somewhere Place"],["Pasta"]]
+    //var numberOfPlaces:[Places] = [Places(placeName: "Mc D"),Places(placeName: "123 Fake Street")]
     
-    var numberOfPlaces:[Places] = [Places(placeName: "Mc D"),Places(placeName: "123 Fake Street")]
-    
+    var placesArray = [Placeslist]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loadItems()
         // we use self here to avoid changing the tab bar item title as well.
         //see:stackoverflow.com/questions/25167458/changing-navigation-title-programmatically
         //self.navigationItem.title = "Select 3 choices"
@@ -31,7 +33,7 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0 {
             
-            return numberOfPlaces.count
+            return placesArray.count
         } else
         {
             return 0
@@ -43,11 +45,11 @@ class TableViewController: UITableViewController {
         
         // create cell from dequeue pile and bind to "Cell" identifier from TableView
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let place = numberOfPlaces[indexPath.row]
+        let place = placesArray[indexPath.row]
         // add place title
-        cell.textLabel?.text = "\(place.placeName)"
+        cell.textLabel?.text = place.placeName
         //cell.textLabel?.text = dummyData[indexPath.row][0]
-
+        
         // if location exists also add to cell description
         //cell.detailTextLabel?.text = dummyData[indexPath.row].count > 1 ? dummyData[indexPath.row][1] : ""
         
@@ -59,6 +61,21 @@ class TableViewController: UITableViewController {
         return .delete
     }
     
+    /* below code to implement later
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedObject = self.numberOfPlaces[sourceIndexPath.row]
+        numberOfPlaces.remove(at: sourceIndexPath.row)
+        numberOfPlaces.insert(movedObject, at: destinationIndexPath.row)
+        debugPrint("\(sourceIndexPath.row) => \(destinationIndexPath.row)")
+        // To check for correctness enable: self.tableView.reloadData()
+    }
+    */
+    
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
     
     //This below method is for pushing to editPlacesTableViewController upon selecting cell from tableviewcontroller 
     
@@ -66,27 +83,80 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            numberOfPlaces.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            Places.saveToFile(selectedPlaces: numberOfPlaces)
+            //Added alert when deleting from list to confirm
+            let refreshAlert = UIAlertController(title: "Confirm Delete", message: "Do you want to delete this Item?", preferredStyle: UIAlertController.Style.alert)
+            
+            refreshAlert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action: UIAlertAction!) in
+                //perform your action
+                
+                //self.numberOfPlaces.remove(at: indexPath.row)
+                //tableView.deleteRows(at: [indexPath], with: .automatic)
+                self.context.delete(self.placesArray[indexPath.row])
+                self.placesArray.remove(at: indexPath.row)
+                
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+               // Places.saveToFile(selectedPlaces: self.numberOfPlaces)
+            }))
+            
+            refreshAlert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
+            
+            
+            
+            present(refreshAlert, animated: true, completion: nil)
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let place = placesArray[indexPath.row]
+        performSegue(withIdentifier: "editPlace", sender: place)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "editPlace"
         {
             let indexPath = tableView.indexPathForSelectedRow!
-            let places = numberOfPlaces[indexPath.row]
+            let places = placesArray[indexPath.row]
             let navController = segue.destination as! UINavigationController
             let editAddedPlacesTableViewController = navController.topViewController  as! editPlacesTableViewController
-            editAddedPlacesTableViewController.selectedPlace = places
+            editAddedPlacesTableViewController.selectedPlace  = places.placeName ?? ""
+            let editAddedPlacesTableViewControllerIndexPath = navController.topViewController as! editPlacesTableViewController
+            editAddedPlacesTableViewControllerIndexPath.selectedIndexpath = indexPath.row
+            
+            
         }
+    }
+    
+    func loadItems() {
+        let request : NSFetchRequest<Placeslist> = Placeslist.fetchRequest()
+        do {
+            placesArray = try context.fetch(request)
+        } catch
+        {
+            print("Error fetching data from context \(error)")
+        }
+        
+    }
+    
+    
+    
+    //To reload list of places tableview
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        loadItems()
+        tableView.reloadData()
+    }
+    @IBAction func editPressed(_ sender: UIBarButtonItem) {
+        
+        self.isEditing = !self.isEditing
+
     }
     
     @IBAction func unwindToTableView(segue: UIStoryboardSegue) {
         guard segue.identifier == "saveUnwind" else {return}
-        let sourceViewController = segue.source as! editPlacesTableViewController
+        /*let sourceViewController = segue.source as! editPlacesTableViewController
         
         if let selectedPlace = sourceViewController.selectedPlace {
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
@@ -97,7 +167,7 @@ class TableViewController: UITableViewController {
                 tableView.insertRows(at:[newIndexPath], with: .automatic)
             }
             Places.saveToFile(selectedPlaces: numberOfPlaces)
-        }
+        }*/
         
     }
 
--- dinnerApp/dinnerApp/editPlacesTableViewController.swift
+++ dinnerApp/dinnerApp/editPlacesTableViewController.swift
@@ -7,20 +7,28 @@
 //
 
 import UIKit
+import CoreData
 
 class editPlacesTableViewController: UITableViewController,UITextViewDelegate {
-var selectedPlace: Places!
+//var selectedPlace: Places!
+  var newPlaces = [Placeslist]()
+    
+    var selectedPlace = ""
+    var selectedIndexpath = 0 
+    
+    
     @IBOutlet weak var placeDisplay: UITextField!
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
-    
-    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    //var appDel = (UIApplication.shared.delegate as! AppDelegate)
+    //var fetchedResultsController: NSFetchedResultsController!
     override func viewDidLoad() {
     
         super.viewDidLoad()
-        if let selectedPlace = selectedPlace {
-            placeDisplay?.text = selectedPlace.placeName
-        }
+       
+            placeDisplay?.text = selectedPlace
+     
       updateSaveButtonState()
 
     }
@@ -29,15 +37,61 @@ var selectedPlace: Places!
         let placeDisplayText = placeDisplay.text ?? ""
         saveButton.isEnabled = !placeDisplayText.isEmpty
     }
+    
+    
 
     @IBAction func textEditChanged(_ sender: UITextField) {
         updateSaveButtonState()
     }
     
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        super.prepare(for: segue, sender: sender)
-        guard segue.identifier == "saveUnwind" else {return}
-        let newPlace = placeDisplay.text ?? ""
-        selectedPlace = Places(placeName: newPlace)
+        if selectedPlace.isEmpty {
+            super.prepare(for: segue, sender: sender)
+            guard segue.identifier == "saveUnwind" else {return}
+            let newPlace = Placeslist(context: self.context)
+            newPlace.placeName = placeDisplay.text!
+            self.savePlaces()
+            self.newPlaces.append(newPlace)
+        }
+        else {
+            updatePlaces()
+        }
+        
+       
     }
+    func updatePlaces() {
+  
+       let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Placeslist")
+        
+        request.predicate = NSPredicate(format: "placeName contains %@", selectedPlace)
+        do {
+            let test = try context.fetch(request)
+            let objectUpdate = test[0] as! NSManagedObject
+            objectUpdate.setValue(placeDisplay.text, forKey: "placeName")
+            do {
+                try context.save()
+            }catch
+            {
+                print(error)
+            }
+        } catch
+        {
+            print(error)
+        }
+      
+        
+    }
+    func savePlaces() {
+    
+        do {
+            try context.save()
+        } catch{
+            print("Error saving content \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
 }
