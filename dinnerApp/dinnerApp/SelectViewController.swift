08689f347cae294cc11e1f42de1e34d45da91d29
Thomas Miles <tommylmiles@gmail.com>
2019-10-14 01:17:56 -0400

    Updates added
    Core data added
    Fetching data from coredata and ready for display

--- dinnerApp/dinnerApp/SelectViewController.swift
+++ dinnerApp/dinnerApp/SelectViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import CoreData
 
 class SelectViewController: UIViewController {
-
+    var randomNum = 0
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
    
     override func viewDidLoad() {
         super.viewDidLoad()
                   
     }
 
+    @IBAction func selectButtonTapped(_ sender: Any) {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Placeslist")
+        request.returnsObjectsAsFaults = false
+        var allPlaces = [String]()
+        do {
+            let result = try context.fetch(request)
+            let data = result as! [NSManagedObject]
+            allPlaces = data.map({placesList in placesList.value(forKey: "placeName") as! String})
+            //print(allPlaces)
+            if allPlaces.isEmpty {
+                let refreshAlert = UIAlertController(title: "Hellooo", message: "There are no places to select, Please add places from the places tab", preferredStyle: UIAlertController.Style.alert)
+                refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+                present(refreshAlert, animated: true, completion: nil)
+                //print("No Places Available in the list, add the places to the list")
+            } else {
+                let randomNum = Int(arc4random_uniform(UInt32(allPlaces.count)))
+                let finalPlaceSelected = "\(allPlaces[randomNum])"
+                let refreshAlert = UIAlertController(title: "Lets goto", message: "\(finalPlaceSelected)", preferredStyle: UIAlertController.Style.alert)
+                refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+                present(refreshAlert, animated: true, completion: nil)
+                print(finalPlaceSelected)
+                }
+            } catch
+        {
+            print("Failed")
+        }
+        
+    }
+    
+    
 }
 
--- dinnerApp/dinnerApp/editPlacesTableViewController.swift
+++ dinnerApp/dinnerApp/editPlacesTableViewController.swift
@@ -7,19 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
 class editPlacesTableViewController: UITableViewController,UITextViewDelegate {
-var selectedPlace: Places!
+//var selectedPlace: Places!
+  var newPlaces = [Placeslist]()
+    
     @IBOutlet weak var placeDisplay: UITextField!
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
-    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
     
         super.viewDidLoad()
-        if let selectedPlace = selectedPlace {
-            placeDisplay?.text = selectedPlace.placeName
+        if let selectedPlace = placeDisplay {
+            placeDisplay?.text = selectedPlace.text
         }
       updateSaveButtonState()
 
@@ -34,10 +37,24 @@ var selectedPlace: Places!
         updateSaveButtonState()
     }
     
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         guard segue.identifier == "saveUnwind" else {return}
-        let newPlace = placeDisplay.text ?? ""
-        selectedPlace = Places(placeName: newPlace)
+        let newPlace = Placeslist(context: self.context)
+        newPlace.placeName = placeDisplay.text!
+        self.savePlaces()
+        self.newPlaces.append(newPlace)
     }
+    
+    func savePlaces() {
+        do {
+            try context.save()
+        } catch{
+            print("Error saving content \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
 }
